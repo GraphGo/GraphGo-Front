30b0a9737805c86cfe9c4e8bf84c36c83272aea7
bryanyuan1 <yiyuan@ucsd.edu>
2020-10-26 11:35:29 -0700

    put pad into the basic UI

--- src/views/CanvasPage/CanvasPage.js
+++ src/views/CanvasPage/CanvasPage.js
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import ToolBar from "../../components/ToolBar/ToolBar";
 import classes from "./CanvasPage.module.css";
+import styled from "styled-components";
+
 const CanvasPage = () => {
   return (
     <div>
-      <Header canvasPage={true} />
+      <Header canvasPage={true}/>
       <ToolBar />
+      <DrawingArea src="./demo.html"></DrawingArea>
     </div>
   );
 };
+
+const DrawingArea = styled.iframe`
+  width: 100%;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  height: 90vh;
+  box-sizing: border-box;
+`;
 
 export default CanvasPage;
