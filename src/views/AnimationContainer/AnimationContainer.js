5011120f5f8c2eda76d92d4307e24afb8e5de0bd
zehuafu <fuzehua2000@gmail.com>
2020-11-15 18:19:02 -0800

    added displaying formatted array feature

--- src/views/AnimationContainer/AnimationContainer.js
+++ src/views/AnimationContainer/AnimationContainer.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 class AnimationContainer extends Component {
+  constructor(inputArray) {
+    super();
+    array = [];
+    for (i = 0; i < inputArray.length; i++) {
+      array.append({value:inputArray[i], x:(i+1)*15, y:15, speed:{x:1, y:1}})
+    }
+    this.state = {
+      arr: array
+    }
+  }
+  componentDidMount() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    context.font = "20px Georgia";
+    context.fillText('\[', 0, 15);
+    // draw every element in arr
+    for (i = 0; i < this.state.arr.length; i++){
+      object = this.state.arr[i];
+      context.fillText(object.value, object.x, object.y);
+    }
+    context.fillText('\]', this.state.arr[this.state.arr.length-1] + 15, 15);
+  }
+
+  // used to move an element up
+
   render() {
     return (
       <div style={{
