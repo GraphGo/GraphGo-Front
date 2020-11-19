31c636e6969ce5a8bdb2139fae5bb97c7cbb9cde
liuhanshu2000 <liuhanshu2000@hotmail.com>
2020-11-18 19:47:00 -0800

    dynamic content for CMS (not done)

--- src/views/CMS/CMS.js
+++ src/views/CMS/CMS.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import { Grid } from '@material-ui/core'
-import { File, getAllFiles } from '../../API/file'
+import { Grid, Button } from '@material-ui/core'
+import { getAllFiles, getFileById } from '../../API/file'
 import FolderItem from '../../components/FolderItem/FolderItem'
 import FileItem from '../../components/FileItem/FileItem'
 
@@ -9,12 +9,14 @@ class CMS extends React.Component {
     constructor(props) {
         super(props) 
         this.files = [] // all files, reload using loadData
+        this.fileItems = []
         this.currentFolder = null // current path
-        this.onFolderClick.bind(this)
+        this.loadData = this.loadData.bind(this)
     }
 
     componentDidMount(){
         // TODO: use user email from Context
+        console.log("[CMS] --- Component loaded")
         this.loadData()
     }
 
@@ -22,45 +24,57 @@ class CMS extends React.Component {
 
     }
 
+    loadTestData(){
+
+    }   
+
     loadData(){
+        console.log("[CMS] --- Reloading data")
         getAllFiles("liuhanshu2000@gmail.com").then(res => {
             this.files = res
+            console.log(res)
         }).catch(e => console.error(e))
+        this.renderFiles()
     }
 
     onFolderClick() {
         console.log("Clicked")
     }
 
+    renderFiles(){
+        if(!this.currentFolder){
+            this.fileItems = this.files.map(item => {
+                console.log(item)
+                if(item.type === 'folder'){
+                    return (
+                        <Grid item xs={2}>
+                            <FolderItem name={item.name} />
+                        </Grid>
+                    )
+                }else if (item.type === 'graph'){
+                    return (
+                        <Grid item xs={2}>
+                            <FileItem name={item.name}></FileItem>
+                        </Grid>
+                    )
+                }
+            })
+        }
+    }
+
 
     render() {
         const gridStyle = {
-            "margin-top": '35px'
-        };
+            'marginTop': '35px'
+        }
 
         return (
             <>
                 <Header />
                 <div className="main-page">
+                    <Button variant='contained' color='secondary' onClick={() => this.loadData()}>Refresh</Button>
                     <Grid style={gridStyle} container direction='row' justify='flex-start' alignItems='flex-start'>
-                        <Grid style={gridStyle} item xs={2}>
-                            <FolderItem name="new folder" folderClick={this.onFolderClick} />
-                        </Grid>
-                        <Grid style={gridStyle} item xs={2}>
-                            <FileItem name='folder 2' />
-                        </Grid>
-                        <Grid style={gridStyle} item xs={2}>
-                            <FolderItem name="new folder" />
-                        </Grid>
-                        <Grid style={gridStyle}item xs={2}>
-                            <FolderItem name="new folder" />
-                        </Grid>
-                        <Grid style={gridStyle}item xs={2}>
-                            <FolderItem name="Folder with a really really long name and everything and really really long name so long" />
-                        </Grid>
-                        <Grid style={gridStyle}item xs={2}>
-                            <FolderItem name="new folder" />
-                        </Grid>
+                        {this.fileItems}
                     </Grid>
                 </div>
             </>
